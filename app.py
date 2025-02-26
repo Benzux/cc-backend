8a02a680277590f20a546a623d006c210f96cab2
Benjamin Lukin <180255353+Benzux@users.noreply.github.com>
2025-02-26 14:49:52 +0200

    test 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello World! itekki</h1>"
+    return "<h1>Hello World! itekki jee</h1>"
 
 @app.route("/sentiment", methods=['POST'])
 def get_sentiment():
